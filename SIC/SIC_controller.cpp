266191eb8986d3ea9efc5fdbf8922b2220bff1cb
whitecloudy <alex9395@naver.com>
2021-03-11 20:59:19 +0900

    Fix SIC

--- SIC/SIC_controller.cpp
+++ SIC/SIC_controller.cpp
@@ -16,22 +16,16 @@ SIC_controller::SIC_controller(std::complex<float> input_ref){
 
 int SIC_controller::setCurrentAmp(std::complex<float> amp_Rx){
   std::complex<float> amp_Ant = amp_Rx - SIC_cha * weight_cur;
-  std::complex<float> weight_new = -amp_Ant/SIC_cha;
+  weight_cur = -amp_Ant/SIC_cha;
 
   
 #ifdef  _SIC_DEBUG_
   std::cout<<"ANT amp : "<<amp_Ant<<std::endl;
   std::cout<<"SIC channel : "<<SIC_cha<<std::endl;
-  std::cout<<"weight_new : "<<weight_new<<std::endl;
-  std::cout<<"new dB : "<<Amp2dB(std::abs(weight_new))<<std::endl;
-#endif
-  weight_cur = weight_new;
-
-#ifdef  _SIC_DEBUG_
-  std::cout<<"weight_true : "<<weight_new<<std::endl;
+  std::cout<<"weight_cur : "<<weight_new<<std::endl;
+  std::cout<<"new dB : "<<Amp2dB(std::abs(weight_cur))<<std::endl;
   std::cout<<"phase : "<<Rad2Deg(std::arg(weight_cur))<<std::endl;
-  std::cout<<"true dB : "<<Amp2dB(std::abs(weight_new))<<std::endl;
-  std::cout<<"Expected : "<<amp_Ant + weight_new*SIC_cha<<std::endl;
+  std::cout<<"Expected : "<<amp_Ant + weight_cur*SIC_cha<<std::endl;
 #endif
 
   return 0;
